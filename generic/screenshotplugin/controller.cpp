aff6a65b89ee2933e85afaf0761048e4072033b8
wadealer <wadealer@gmail.com>
2012-02-22 11:19:12 +0200

    fix regexp for omploader in screenshotplugin

--- generic/screenshotplugin/controller.cpp
+++ generic/screenshotplugin/controller.cpp
@@ -33,7 +33,7 @@ static const QString pixacadem = "Pix.Academ.org&split&http://pix.academ.org/&sp
 static const QString kachalka = "Kachalka.com&split&http://www.kachalka.com/upload.php&split&&split&&split&&split&userfile[]&split&name=\"option\" value=\"(http://www.kachalka.com/[^\"]+)\" /></td>&split&true";
 static const QString flashtux = "Img.Flashtux.org&split&http://img.flashtux.org/index.php&split&&split&&split&postimg=1&split&filename&split&<br />Link: <a href=\"(http://img.flashtux.org/[^\"]+)\">&split&true";
 static const QString smages = "Smages.com&split&http://smages.com/&split&&split&&split&&split&fileup&split&<div class=\"codex\"><a href=\"(http://smages.com/[^\"]+)\" target=\"_blank\">URL:</a></div>&split&true";
-static const QString ompldr = "Ompldr.org&split&http://ompldr.org/upload&split&&split&&split&&split&file1&split&<div class=\"left\">File:</div><div class=\"right\"><a href=\"[^\"]+\">(http://ompldr.org/[^\"]+)</a></div>&split&true";
+static const QString ompldr = "Omploader.org&split&http://omploader.org/upload&split&&split&&split&&split&file1&split&<div class=\"left\">File:</div><div class=\"right\"><a href=\"[^\"]+\">(http://omploader.org/[^\"]+)</a></div>&split&true";
 static const QString ipicture = "Ipicture.ru&split&http://ipicture.ru/Upload/&split&&split&&split&method=file&split&userfile&split&value=\"(http://[^\"]+)\">&split&true";
 
 static const QStringList staticHostsList = QStringList() << imageShack << pixacadem << radikal
