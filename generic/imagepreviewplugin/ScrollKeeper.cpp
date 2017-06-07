psi-im/plugins

commit 146b1a5f38ea18a12bf23d989f27c1198cc6dac8

fixed juick and image preview plugins compilation with webengine. todo: add webengine support for these plugins

generic/imagepreviewplugin/ScrollKeeper.cpp
@@ -6,13 +6,28 @@
  */
 
 #include "ScrollKeeper.h"
+#ifdef HAVE_WEBKIT
 #include <QWebView>
+#else
+#ifdef __GNUC__
+#warning "ImagePreviewPlugin TODO: add support for webengine"
+#endif
+#endif
 #include <QScrollBar>
 
 //#define SCROLL_DEBUG
 
 ScrollKeeper::ScrollKeeper(QWidget* chatView) :
-		chatView_(chatView), scrollPos_(0), scrollToEnd_(false), ted_(0), mainFrame_(0) {
+		chatView_(chatView),
+		scrollPos_(0),
+		scrollToEnd_(false),
+		ted_(0)
+#ifdef HAVE_WEBKIT
+		,mainFrame_(0)
+#endif
+{
+	Q_UNUSED(chatView_)
+
 	ted_ = qobject_cast<QTextEdit*>(chatView);
 	if (ted_) {
 		scrollPos_ = ted_->verticalScrollBar()->value();
@@ -23,7 +38,9 @@ ScrollKeeper::ScrollKeeper(QWidget* chatView) :
 		qDebug() << "QTED Scroll pos:" << scrollPos_ << "to end:" << scrollToEnd_ << "max:"
 				<< ted_->verticalScrollBar()->maximum();
 #endif
-	} else {
+	}
+#ifdef HAVE_WEBKIT
+	else {
 		QWebView* wv = qobject_cast<QWebView*>(chatView);
 		if (!wv) {
 			return;
@@ -38,6 +55,8 @@ ScrollKeeper::ScrollKeeper(QWidget* chatView) :
 				<< mainFrame_->scrollBarMaximum(Qt::Vertical) << "min:" << mainFrame_->scrollBarMinimum(Qt::Vertical);
 #endif
 	}
+#else
+#endif
 }
 
 ScrollKeeper::~ScrollKeeper() {
@@ -48,6 +67,7 @@ ScrollKeeper::~ScrollKeeper() {
 #endif
 		ted_->verticalScrollBar()->setValue(scrollToEnd_ ? ted_->verticalScrollBar()->maximum() : scrollPos_);
 	}
+#ifdef HAVE_WEBKIT
 	if (mainFrame_) {
 #ifdef SCROLL_DEBUG
 		qDebug() << "QWV restoring scroll pos:" << scrollPos_ << "to end:" << scrollToEnd_ << "max:"
@@ -55,5 +75,6 @@ ScrollKeeper::~ScrollKeeper() {
 #endif
 		mainFrame_->setScrollBarValue(Qt::Vertical,	scrollToEnd_ ? mainFrame_->scrollBarMaximum(Qt::Vertical) : scrollPos_);
 	}
+#endif
 }
 

generic/imagepreviewplugin/ScrollKeeper.h
@@ -10,7 +10,8 @@
 
 #include <QWidget>
 #include <QTextEdit>
-#include <QWebFrame>
+
+class QWebFrame;
 
 class ScrollKeeper {
 private:
@@ -18,7 +19,9 @@ private:
 	int scrollPos_;
 	bool scrollToEnd_;
 	QTextEdit* ted_;
+#ifdef HAVE_WEBKIT
 	QWebFrame* mainFrame_;
+#endif
 public:
 	ScrollKeeper(QWidget* chatView);
 	virtual ~ScrollKeeper();

generic/imagepreviewplugin/imagepreviewplugin.cpp
@@ -44,9 +44,11 @@
 #include <QSpinBox>
 #include <QComboBox>
 #include <QCheckBox>
+#ifdef HAVE_WEBKIT
 #include <QWebView>
 #include <QWebFrame>
 #include <QWebElementCollection>
+#endif
 #include <stdexcept>
 
 #undef AUTOREDIRECTS
@@ -251,7 +253,9 @@ void ImagePreviewPlugin::messageAppended(const QString &, QWidget* logWidget) {
 			queueUrl(url, new Origin(te_log));
 		};
 		te_log->setTextCursor(cur);
-	} else {
+	}
+#ifdef HAVE_WEBKIT
+	else {
 		QWebView* wv_log = qobject_cast<QWebView*>(logWidget);
 		QWebFrame* mainFrame = wv_log->page()->mainFrame();
 		QWebElementCollection elems = mainFrame->findAllElements("a[href]");
@@ -269,6 +273,7 @@ void ImagePreviewPlugin::messageAppended(const QString &, QWidget* logWidget) {
 			}
 		}
 	}
+#endif
 }
 
 void ImagePreviewPlugin::imageReply(QNetworkReply* reply) {
@@ -362,7 +367,9 @@ void ImagePreviewPlugin::imageReply(QNetworkReply* reply) {
 					}
 				}
 				te_log->setTextCursor(saved);
-			} else {
+			}
+#ifdef HAVE_WEBKIT
+			else {
 				QByteArray imageBytes;
 				QBuffer imageBuf(&imageBytes);
 				image.save(&imageBuf, "jpg", 60);
@@ -374,6 +381,7 @@ void ImagePreviewPlugin::imageReply(QNetworkReply* reply) {
 						"  elem.innerHTML = \"<img src='data:image/jpeg;base64,%2'/>\";"
 						"}").arg(urlStr).arg(QString(imageBytes.toBase64())));
 			}
+#endif
 		} catch (std::exception& e) {
 			failed.insert(origin->originalUrl_);
 			qWarning() << "ERROR: " << e.what();

generic/imagepreviewplugin/imagepreviewplugin.pro
@@ -1,9 +1,19 @@
-#CONFIG += release
-QT += webkit network
+CONFIG += release
+QT += network
 greaterThan(QT_MAJOR_VERSION, 4) {
-    QT += webkitwidgets
-    CONFIG += c++11
+    greaterThan(QT_MINOR_VERSION, 5) {
+        QT += webengine webenginewidgets
+        DEFINES += HAVE_WEBENGINE
+    }
+    else {
+        QT += webkit webkitwidgets
+        DEFINES += HAVE_WEBKIT
+    }
 } 
+else {
+    QT += webkit
+    DEFINES += HAVE_WEBKIT
+}
 
 isEmpty(PSISDK) {
     include(../../psiplugin.pri)

generic/juickplugin/juickplugin.cpp
@@ -22,9 +22,15 @@
 #include <QMessageBox>
 #include <QColorDialog>
 #include <QTextEdit>
+#ifdef HAVE_WEBKIT
 #include <QWebView>
 #include <QWebFrame>
 #include <QWebElement>
+#else
+#ifdef __GNUC__
+#warning "JuickPlugin TODO: add support for webengine"
+#endif
+#endif
 
 #include "optionaccessinghost.h"
 #include "activetabaccessinghost.h"
@@ -1104,6 +1110,7 @@ void JuickPlugin::updateWidgets(const QList<QByteArray>& urls)
 //			te->update();
 			te->setLineWrapColumnOrWidth(te->lineWrapColumnOrWidth());
 		}
+#ifdef HAVE_WEBKIT
 		else {
 			QWebView *wv = w->findChild<QWebView*>();
 			if(wv) {
@@ -1118,6 +1125,7 @@ void JuickPlugin::updateWidgets(const QList<QByteArray>& urls)
 				}
 			}
 		}
+#endif
 	}
 }
 

generic/juickplugin/juickplugin.pro
@@ -14,9 +14,20 @@ HEADERS += \
     defines.h \
     juickplugin.h
 
-QT += network webkit
+QT += network
 greaterThan(QT_MAJOR_VERSION, 4) {
-  QT += webkitwidgets
+    greaterThan(QT_MINOR_VERSION, 5) {
+        QT += webengine webenginewidgets
+        DEFINES += HAVE_WEBENGINE
+    }
+    else {
+        QT += webkit webkitwidgets
+        DEFINES += HAVE_WEBKIT
+    }
+}
+else {
+    QT += webkit
+    DEFINES += HAVE_WEBKIT
 }
 
 FORMS += \

commit cd5e61a948bc16c952ea535ba45b66b994aa8cb5

imagepreviewplugin: include QWebFrame
To fix build:
src/plugins/generic/imagepreviewplugin/ScrollKeeper.cpp:48:26: error: invalid use of incomplete type 'class QWebFrame'
  scrollPos_ = mainFrame_->scrollBarValue(Qt::Vertical);
                           ^~~~~~~~~~~~~~

This was broken by 146b1a5f38ea18a12bf23d989f27c1198cc6dac8.

Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

generic/imagepreviewplugin/ScrollKeeper.cpp
@@ -7,6 +7,7 @@
 
 #include "ScrollKeeper.h"
 #ifdef HAVE_WEBKIT
+#include <QWebFrame>
 #include <QWebView>
 #endif
 #ifdef HAVE_WEBENGINE