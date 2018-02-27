ac83b7a9861a13cf226d59996b8df639f1c22234
Vyacheslav Karpukhin <vyacheslav@karpukhin.com>
2018-02-27 12:08:32 +0100

    OMEMO: more MSVC compatibility

--- dev/omemoplugin/src/crypto.cpp
+++ dev/omemoplugin/src/crypto.cpp
@@ -26,18 +26,18 @@ using namespace QCA;
 namespace psiomemo {
   void Crypto::initCryptoProvider(signal_context *ctx) {
     signal_crypto_provider crypto_provider = {
-        .random_func = random,
-        .hmac_sha256_init_func = hmac_sha256_init,
-        .hmac_sha256_update_func = algo_update,
-        .hmac_sha256_final_func = algo_final,
-        .hmac_sha256_cleanup_func = algo_cleanup,
-        .sha512_digest_init_func = sha512_digest_init,
-        .sha512_digest_update_func = algo_update,
-        .sha512_digest_final_func = algo_final,
-        .sha512_digest_cleanup_func = algo_cleanup,
-        .encrypt_func = aes_encrypt,
-        .decrypt_func = aes_decrypt,
-        .user_data = nullptr
+        /*.random_func =*/ random,
+        /*.hmac_sha256_init_func =*/ hmac_sha256_init,
+        /*.hmac_sha256_update_func =*/ algo_update,
+        /*.hmac_sha256_final_func =*/ algo_final,
+        /*.hmac_sha256_cleanup_func =*/ algo_cleanup,
+        /*.sha512_digest_init_func =*/ sha512_digest_init,
+        /*.sha512_digest_update_func =*/ algo_update,
+        /*.sha512_digest_final_func =*/ algo_final,
+        /*.sha512_digest_cleanup_func =*/ algo_cleanup,
+        /*.encrypt_func =*/ aes_encrypt,
+        /*.decrypt_func =*/ aes_decrypt,
+        /*.user_data =*/ nullptr
     };
 
     signal_context_set_crypto_provider(ctx, &crypto_provider);
