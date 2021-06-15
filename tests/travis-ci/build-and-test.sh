708b15ee5db42f9632b46d1fdc422d9d0ecfcdbe
Boris Pek <tehnick-8@yandex.ru>
2021-06-16 01:55:31 +0300

    Travis CI: test builds for 64-bit MS Windows using MinGW compiller
    and build environment from MXE project. See: https://mxe.cc/

--- tests/travis-ci/build-and-test.sh
+++ tests/travis-ci/build-and-test.sh
@@ -20,6 +20,13 @@ elif [ "${TARGET}" = "macos64" ]
 then
     ./tests/travis-ci/build-in-macos.sh
 fi
+elif [ "${TARGET}" = "windows64" ]
+then
+    ./tests/travis-ci/build-for-windows.sh
+else
+    echo "Unknown target!"
+    exit 1
+fi
 
 cd ./builddir/psi/plugins/
 ls -alp *
