d403548f2b7d5347d9d6861e15e316f006fe41b0
Boris Pek <tehnick-8@yandex.ru>
2021-06-18 00:34:22 +0300

    Add forgotten script to repo

---
+++ tests/travis-ci/build-for-windows.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Author:  Boris Pek
+# Version: N/A
+# License: Public Domain
+
+set -e
+set -x
+
+[ -z "${BUILD_DEV_PLUGINS}" ] && BUILD_DEV_PLUGINS="OFF"
+
+BUILD_OPTIONS="-DCMAKE_BUILD_TYPE=Release \
+               -DBUILD_DEV_PLUGINS=${BUILD_DEV_PLUGINS} \
+               "
+
+mkdir -p builddir
+cd builddir
+
+if [ "${TARGET}" = "windows64" ]
+then
+    export PREFIX="x86-64-w64-mingw32.shared"
+else
+    echo "Unknown target!"
+    exit 1
+fi
+
+${PREFIX}-cmake .. ${BUILD_OPTIONS}
+make -k -j $(nproc) VERBOSE=1
+
