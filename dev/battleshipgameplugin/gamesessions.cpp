psi-im/plugins

commit 3153e16e1c37733ad9e19c56bff3d4e63e5acf26

Fix compiller warnings:
this statement may fall through [-Wimplicit-fallthrough=]

dev/battleshipgameplugin/gamesessions.cpp
@@ -186,6 +186,7 @@ bool GameSessionList::processIncomingIqStanza(int account, const QDomElement &xm
                     }
                     break;
                 case GameSession::StageInitBoard:
+                    break;
                 case GameSession::StageShowBoard:
                     if (gs->status_ == GameSession::StatusWaitBoardVerification)
                     {
@@ -193,14 +194,17 @@ bool GameSessionList::processIncomingIqStanza(int account, const QDomElement &xm
                         gs->status_ = GameSession::StatusNone;
                         err = false;
                     }
+                    break;
                 case GameSession::StageShooting:
                     if (gs->status_ == GameSession::StatusWaitShotConfirmation)
                     {
                         gs->status_ = GameSession::StatusNone;
                         err = !gs->handleTurnResult(xml);
                     }
+                    break;
                 case GameSession::StageEnd:
                     err = false;
+                    break;
                 default:
                     break;
                 }

generic/cleanerplugin/models.cpp
@@ -238,39 +238,45 @@ QVariant ClearingModel::data(const QModelIndex & index, int role) const
     filename = filename.replace("%5f", "_");
     filename = filename.replace("%2d", "-");
     filename = filename.replace("%25", "@");
-    switch(i) {
+    switch(i)
+    {
     case(0):
-                if (role == Qt::CheckStateRole) {
+        if (role == Qt::CheckStateRole) {
             return isSelected(index) ? 2:0;
-                } else if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-                        return QVariant("");
-             case(1):
-                if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole) {
+        } else if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant();
+        break;
+    case(1):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole) {
             if(filename.contains("_at_"))
                 return QVariant(filename.split("_at_").first());
             else
                 return QVariant();
-                }
-             case(2):
-                if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-            return QVariant(filename.split("_at_").last());
-             case(3):
-        if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-            return QVariant(fileSize(index));
-             case(4):
-        if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-            return QVariant(fileDate(index));
         }
+        break;
+    case(2):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant(filename.split("_at_").last());
+        break;
+    case(3):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant(fileSize(index));
+        break;
+    case(4):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant(fileDate(index));
+        break;
+    }
     return QVariant();
 }
 
@@ -358,32 +364,36 @@ QVariant ClearingAvatarModel::data(const QModelIndex & index, int role) const
     QString filename = filePass(index);
     switch(i) {
     case(0):
-                if (role == Qt::CheckStateRole) {
+        if (role == Qt::CheckStateRole) {
             return isSelected(index) ? 2:0;
-                } else if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-                        return QVariant("");
-             case(1):
-                if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole) {
+        } else if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant();
+        break;
+    case(1):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole) {
             QPixmap pix = QPixmap(filename);
             if(pix.isNull())
                 return QVariant();
             return QVariant(pix);
-                }
-             case(2):
-        if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-            return QVariant(fileSize(index));
-             case(3):
-        if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-            return QVariant(fileDate(index));
         }
+        break;
+    case(2):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant(fileSize(index));
+        break;
+    case(3):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant(fileDate(index));
+        break;
+    }
     return QVariant();
 }
 
@@ -415,26 +425,29 @@ QVariant ClearingOptionsModel::data(const QModelIndex & index, int role) const
     int i = index.column();
     switch(i) {
     case(0):
-                if (role == Qt::CheckStateRole) {
+        if (role == Qt::CheckStateRole) {
             return isSelected(index) ? 2:0;
-                } else if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole)
-                        return QVariant("");
-             case(1):
-                if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignLeft | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole) {
-                        return QVariant(options.at(index.row()));
-                }
-             case(2):
-                if (role == Qt::TextAlignmentRole) {
-                        return (int)(Qt::AlignLeft | Qt::AlignVCenter);
-                } else if (role == Qt::DisplayRole) {
-                        QDomNode node = parser_->nodeByString(options.at(index.row()));
-                        return QVariant(node.toElement().text());
-                }
+        } else if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole)
+            return QVariant();
+        break;
+    case(1):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignLeft | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole) {
+            return QVariant(options.at(index.row()));
         }
+        break;
+    case(2):
+        if (role == Qt::TextAlignmentRole) {
+            return (int)(Qt::AlignLeft | Qt::AlignVCenter);
+        } else if (role == Qt::DisplayRole) {
+            QDomNode node = parser_->nodeByString(options.at(index.row()));
+            return QVariant(node.toElement().text());
+        }
+        break;
+    }
     return QVariant();
 }
 

generic/stopspamplugin/model.cpp
@@ -21,8 +21,8 @@
 #include "model.h"
 
 Model::Model(const QStringList &Jids_, QVariantList Sounds_, QObject* parent)
-        : QAbstractTableModel(parent)
-        , Jids(Jids_)
+    : QAbstractTableModel(parent)
+    , Jids(Jids_)
 {
     headers << tr("Enable/Disable") << tr("JID (or part of JID)");
 
@@ -37,12 +37,12 @@ Model::Model(const QStringList &Jids_, QVariantList Sounds_, QObject* parent)
 QVariant Model::headerData ( int section, Qt::Orientation orientation, int role) const
 {
     if (role == Qt::DisplayRole) {
-                if (orientation == Qt::Horizontal) {
-                        return headers.at(section);
-                } else {
-                        return section+1;
-                }
-        } else
+        if (orientation == Qt::Horizontal) {
+            return headers.at(section);
+        } else {
+            return section+1;
+        }
+    } else
         return QVariant();
 }
 
@@ -78,19 +78,20 @@ QVariant Model::data(const QModelIndex & index, int role) const
     int i = index.column();
     switch(i) {
     case(0):
-                if (role == Qt::CheckStateRole)
-                        return selected.contains(tmpJids_.at(index.row())) ? 2:0;
-                else if (role == Qt::TextAlignmentRole)
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                else if (role == Qt::DisplayRole)
-                        return QVariant("");
+        if (role == Qt::CheckStateRole)
+            return selected.contains(tmpJids_.at(index.row())) ? 2:0;
+        else if (role == Qt::TextAlignmentRole)
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        else if (role == Qt::DisplayRole)
+            return QVariant();
+        break;
     case(1):
-                if (role == Qt::TextAlignmentRole)
-                        return (int)(Qt::AlignRight | Qt::AlignVCenter);
-                else if (role == Qt::DisplayRole)
+        if (role == Qt::TextAlignmentRole)
+            return (int)(Qt::AlignRight | Qt::AlignVCenter);
+        else if (role == Qt::DisplayRole)
             return QVariant(tmpJids_.at(index.row()));
-
-        }
+        break;
+    }
     return QVariant();
 }
 

generic/watcherplugin/model.cpp
@@ -89,27 +89,32 @@ QVariant Model::data(const QModelIndex & index, int role) const
         } else if (role == Qt::TextAlignmentRole) {
             return (int)(Qt::AlignRight | Qt::AlignVCenter);
         } else if (role == Qt::DisplayRole)
-            return QVariant("");
+            return QVariant();
+        break;
     case 1:
         if (role == Qt::TextAlignmentRole) {
             return (int)(Qt::AlignRight | Qt::AlignVCenter);
         } else if (role == Qt::DisplayRole)
             return QVariant(tmpWatchedJids_.at(index.row()));
+        break;
     case 2:
         if (role == Qt::TextAlignmentRole) {
             return (int)(Qt::AlignRight | Qt::AlignVCenter);
         } else if (role == Qt::DisplayRole)
             return QVariant(tmpSounds_.at(index.row()));
+        break;
     case 3:
         if (role == Qt::TextAlignmentRole) {
             return (int)(Qt::AlignRight | Qt::AlignVCenter);
         } else if (role == Qt::DisplayRole)
             return QVariant();
+        break;
     case 4:
         if (role == Qt::TextAlignmentRole) {
             return (int)(Qt::AlignRight | Qt::AlignVCenter);
         } else if (role == Qt::DisplayRole)
             return QVariant();
+        break;
     }
     return QVariant();
 }

commit e80410e94a09cec651288a76c3e398bcbeeac979

battleship: fix stage processing
It was broken in 3153e16e1c37733ad9e19c56bff3d4e63e5acf26 when the fall through case was broken by break;

dev/battleshipgameplugin/gamesessions.cpp
@@ -165,13 +165,14 @@ bool GameSessionList::processIncomingIqStanza(int account, const QDomElement &xm
                     }
                     break;
                 case GameSession::StageInitBoard:
-                    break;
-                case GameSession::StageShowBoard:
                     if (gs->status_ == GameSession::StatusWaitBoardVerification) {
                         gs->myBoardChecked_ = true;
                         gs->status_         = GameSession::StatusNone;
                         err                 = false;
                     }
+                    break;
+                case GameSession::StageShowBoard:
+                    err = false;
                     break;
                 case GameSession::StageShooting:
                     if (gs->status_ == GameSession::StatusWaitShotConfirmation) {
@@ -182,7 +183,8 @@ bool GameSessionList::processIncomingIqStanza(int account, const QDomElement &xm
                 case GameSession::StageEnd:
                     err = false;
                     break;
-                default:
+                case GameSession::StageNone:
+                    err = false;
                     break;
                 }
                 if (err)