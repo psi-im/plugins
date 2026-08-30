45f30fb907fe51b976097c349a8858119c7d2330
Sergei Ilinykh <rion4ik@gmail.com>
2026-08-30 21:39:59 +0300

    Restore OTR messaging license header

--- generic/otrplugin/src/otrmessaging.cpp
+++ generic/otrplugin/src/otrmessaging.cpp
@@ -1,5 +1,23 @@
 /*
  * otrmessaging.cpp - Psi-facing OTR messaging interface
+ *
+ * Off-the-Record Messaging plugin for Psi
+ * Copyright (C) 2007-2011  Timo Engel (timo-e@freenet.de)
+ *                    2011  Florian Fieber
+ *                    2014  Boris Pek (tehnick-8@mail.ru)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "otrmessaging.h"
