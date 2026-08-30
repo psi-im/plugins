be62fd858cb3383d18aa3c615a6b71a7e2349cf7
Sergei Ilinykh <rion4ik@gmail.com>
2026-08-30 20:27:09 +0300

    Remove legacy OTR runtime helpers

--- generic/otrplugin/qca-otr/adaptertest/dependencyguard.cmake
+++ generic/otrplugin/qca-otr/adaptertest/dependencyguard.cmake
@@ -6,20 +6,24 @@ file(READ "${PLUGIN_CMAKE}" plugin_cmake)
 
 # The normal plugin deliberately disables qca-otr's optional libotr oracle
 # tests, so the option name itself may contain LIBOTR. Reject only constructs
-# that would actually discover or link the legacy runtime libraries.
+# that would actually discover or link legacy runtime libraries.
 set(forbidden_runtime_references
     "find_package(LIBOTR"
     "find_package(LIBGCRYPT"
     "find_package(LIBGPGERROR"
+    "find_package(LIBTIDY"
     "pkg_check_modules(LIBOTR"
     "pkg_check_modules(GCRYPT"
     "pkg_check_modules(GPGERROR"
+    "pkg_check_modules(TIDY"
     "PkgConfig::LIBOTR"
     "PkgConfig::GCRYPT"
     "PkgConfig::GPGERROR"
+    "PkgConfig::TIDY"
     "\${LIBOTR_"
     "\${LIBGCRYPT_"
     "\${LIBGPGERROR_"
+    "\${LIBTIDY_"
 )
 
 foreach(forbidden IN LISTS forbidden_runtime_references)
--- generic/otrplugin/src/htmltidy.h
+++ generic/otrplugin/src/htmltidy.h
@@ -1,69 +1,24 @@
 /*
- * htmltidy.h - Tidy html with libtidy
- *
- * Off-the-Record Messaging plugin for Psi
- * Copyright (C) 2007-2011  Timo Engel (timo-e@freenet.de)
- *                    2011  Florian Fieber
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- *
+ * htmltidy.h - Normalize rich text with Qt
  */
 
 #ifndef HTMLTIDY_H_
 #define HTMLTIDY_H_
 
-#include <QByteArray>
 #include <QString>
-
-#include <string>
-
-#ifdef LEGACY_TIDY
-#ifdef Q_OS_WIN
-#include <tidy/buffio.h>
-#include <tidy/tidy.h>
-#else
-#include <buffio.h>
-#include <tidy.h>
-#endif
-#else
-#include <tidy.h>
-#include <tidybuffio.h>
-#endif
 
 class QDomDocument;
 class QDomElement;
 
 class HtmlTidy {
 public:
-    HtmlTidy(const QString &html);
-    ~HtmlTidy();
-    QString     output();
-    QDomElement output(QDomDocument &document);
-    static void putByte(void *sinkData, byte bt);
-#ifdef Q_OS_WIN
-    static void TIDY_CALL callPutByte(void *sinkData, byte bt);
-#endif
+    explicit HtmlTidy(QString html);
 
-protected:
-    void    putByte(byte bt);
-    QString writeOutput();
+    QString output() const;
+    QDomElement output(QDomDocument &document) const;
 
 private:
-    TidyDoc    m_tidyDoc;
-    TidyBuffer m_errorOutput;
-    QByteArray m_output;
-    QString    m_input;
+    QString m_input;
 };
 
 #endif
