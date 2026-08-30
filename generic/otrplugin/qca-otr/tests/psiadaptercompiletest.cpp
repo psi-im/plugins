5070a03306dccee1bdb598dcb276384acfc2bbff
Sergei Ilinykh <rion4ik@gmail.com>
2026-08-30 20:36:34 +0300

    Exercise Qt OTR helpers in adapter smoke test

--- generic/otrplugin/qca-otr/tests/psiadaptercompiletest.cpp
+++ generic/otrplugin/qca-otr/tests/psiadaptercompiletest.cpp
@@ -1,6 +1,23 @@
+#include "htmlnormalizer.h"
 #include "otrmessaging.h"
+
+#include <QByteArray>
 
 int main()
 {
+    const QByteArray value = QByteArray::fromHex("00112233445566778899aabbccddeeff00112233");
+    const psiotr::Fingerprint fingerprint(value, QStringLiteral("account"), QStringLiteral("peer"), {});
+    if (!fingerprint.isValid() || fingerprint.value != value || fingerprint.fingerprintHuman.isEmpty())
+        return 1;
+
+    const psiotr::Fingerprint copy = fingerprint;
+    if (!copy.isValid() || copy.value != value)
+        return 2;
+
+    HtmlNormalizer normalizer(QStringLiteral("<body><b>broken <i>rich text</body>"));
+    const QString normalized = normalizer.output();
+    if (!normalized.contains(QStringLiteral("broken")) || !normalized.contains(QStringLiteral("rich text")))
+        return 3;
+
     return 0;
 }
